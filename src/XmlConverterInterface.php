1f324a4522bde83467bfc66a7374400d8da4ea38
Demian <demyan.shtepa@gmail.com>
2018-02-07 19:29:52 +0200

    added check fail

--- src/XmlConverterInterface.php
+++ src/XmlConverterInterface.php
@@ -2,12 +2,16 @@
 
 namespace DemianShtepa\Payture;
 
+use DemianShtepa\Payture\Exceptions\PaytureException;
+
 interface XmlConverterInterface
 {
     /**
      * @param string $xml
      *
      * @return array
+     *
+     * @throws PaytureException
      */
     public function convert(string $xml): array;
 }
